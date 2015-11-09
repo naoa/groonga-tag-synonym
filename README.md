97c11d530b422c8a8535fec4c6e93ffddf2db67c
naoa <visionary.incident@gmail.com>
2015-11-09 21:18:19 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,99 @@
+# Groonga tag synonym plugin
+
+Groongaの参照型カラムへのデータセット時に、投入データと参照先テーブルにある参照型の``synonym``カラムにあるキーと入れ替えることができます。
+
+タグの同義語変換などに利用することができます。
+
+## ```tag_synonym_add```
+
+投入タグに対してを同義語変換したいカラムにHOOKを登録します。
+
+```
+tag_synonym_add --table Memos --column tags
+[[0,0.0,0.0],[0]]
+```
+
+あらかじめタグテーブル(語彙表)にsynonymカラムを作り、変換後の値を入れておくことにより、データ投入時にその値に変換されます。以下の例では、Ruby=Rroongaと登録しておくことにより、RubyがRroongaに変換して登録されています。
+
+```
+plugin_register commands/tag_synonym
+[[0,0.0,0.0],true]
+table_create Tags TABLE_PAT_KEY ShortText
+[[0,0.0,0.0],true]
+column_create Tags synonym COLUMN_SCALAR Tags
+[[0,0.0,0.0],true]
+load --table Tags
+[
+{"_key": "Ruby", "synonym": "Rroonga"}
+]
+[[0,0.0,0.0],1]
+table_create Memos TABLE_HASH_KEY ShortText
+[[0,0.0,0.0],true]
+column_create Memos tags COLUMN_VECTOR Tags
+[[0,0.0,0.0],true]
+tag_synonym_add --table Memos --column tags
+[[0,0.0,0.0],[0]]
+load --table Memos
+[
+{"_key": "Groonga", "tags": ["Groonga"]},
+{"_key": "Rroonga", "tags": ["Groonga", "Ruby"]}
+]
+[[0,0.0,0.0],2]
+select Memos --output_columns 'tags'
+[[0,0.0,0.0],[[[2],[["tags","Tags"]],[["Groonga"]],[["Groonga","Rroonga"]]]]]
+```
+
+### 制限
+HOOKさせるカラムは他のテーブルへの参照型でないといけません。
+また、参照先のテーブルに``synonym``カラムが必要です。
+
+以下のように参照先のテーブルにTokenDelimitトークナイザを指定しておくことにより、入力時は空白区切の文字列であっても構いません。(Mroongaでの利用を想定)
+
+```
+table_create Tags TABLE_PAT_KEY ShortText --default_tokenizer TokenDelimit
+load --table Memos
+[
+{"_key": "Groonga", "tags": "Groonga"},
+{"_key": "Rroonga", "tags": "Groonga Ruby"}
+]
+[[0,0.0,0.0],2]
+select Memos --output_columns 'tags'
+[[0,0.0,0.0],[[[2],[["tags","Tags"]],[["Groonga"]],[["Groonga","Rroonga"]]]]]
+```
+
+## ```tag_synonym_delete```
+カラムに登録したtag_synonymフックを解除します。
+
+```
+tag_synonym_delete --table Memos --column tags
+[[0,0.0,0.0],[0]]
+```
+
+
+## Install
+
+Install libgroonga-dev / groonga-devel
+
+Build this command.
+
+    % sh autogen.sh
+    % ./configure
+    % make
+    % sudo make install
+
+## Usage
+
+Register `commands/tag_synonym`:
+
+    % groonga DB
+    > plugin_register commands/tag_synonym
+
+Now, you can use `tag_synonym_add` and `tag_synonym_delete` command
+
+## Author
+
+Naoya Murakami naoya@createfield.com
+
+## License
+
+LGPL 2.1. See COPYING-LGPL-2.1 for details.
---
+++ commands/tag_synonym.c
@@ -0,0 +1,264 @@
+/* Copyright(C) 2015 Naoya Murakami <naoya@createfield.com>
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License version 2.1 as published by the Free Software Foundation.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+  02110-1301  USA
+*/
+
+#include <string.h>
+#include <groonga/plugin.h>
+
+#ifdef __GNUC__
+# define GNUC_UNUSED __attribute__((__unused__))
+#else
+# define GNUC_UNUSED
+#endif
+
+#define SYNONYM_COLUMN_NAME "synonym"
+#define SYNONYM_COLUMN_NAME_LEN 7
+
+static grn_bool
+is_table(grn_obj *obj)
+{
+  switch (obj->header.type) {
+  case GRN_TABLE_HASH_KEY :
+  case GRN_TABLE_PAT_KEY :
+  case GRN_TABLE_DAT_KEY :
+  case GRN_TABLE_NO_KEY :
+    return GRN_TRUE;
+  default :
+    break;
+  }
+  return GRN_FALSE;
+}
+
+static grn_bool
+is_string(grn_obj *obj)
+{
+  switch (obj->header.type) {
+  case GRN_TYPE :
+    return GRN_TRUE;
+  default :
+    break;
+  }
+  return GRN_FALSE;
+}
+
+static grn_obj *
+command_tag_synonym(grn_ctx *ctx, GNUC_UNUSED int nargs, GNUC_UNUSED grn_obj **args,
+                    GNUC_UNUSED grn_user_data *user_data)
+{
+  GNUC_UNUSED grn_obj *flags = grn_ctx_pop(ctx);
+  grn_obj *newvalue = grn_ctx_pop(ctx);
+  grn_obj *oldvalue = grn_ctx_pop(ctx);
+  GNUC_UNUSED grn_obj *id = grn_ctx_pop(ctx);
+  grn_obj buf;
+  grn_obj record;
+  grn_obj *domain;
+  grn_obj *table;
+  grn_obj *column;
+  int i,n;
+
+  if (GRN_BULK_VSIZE(newvalue) == 0) {
+    return NULL;
+  }
+
+  GRN_TEXT_INIT(&buf, 0);
+
+  table = grn_ctx_at(ctx, oldvalue->header.domain);
+  if (table && !is_table(table)) {
+    GRN_PLUGIN_LOG(ctx, GRN_LOG_WARNING,
+                   "[tag-synonym] "
+                   "hooked column must be reference type");
+    return NULL;
+  }
+
+  column = grn_obj_column(ctx,
+                          table,
+                          SYNONYM_COLUMN_NAME,
+                          SYNONYM_COLUMN_NAME_LEN);
+  if (!column) {
+    GRN_PLUGIN_LOG(ctx, GRN_LOG_WARNING,
+                   "[tag-synonym] "
+                   "couldn't open synonym column");
+    return NULL;
+  }
+
+  domain = grn_ctx_at(ctx, newvalue->header.domain);
+  if (domain && is_string(domain)) {
+      GRN_RECORD_INIT(&record, GRN_OBJ_VECTOR, oldvalue->header.domain);
+    grn_table_tokenize(ctx, table, GRN_TEXT_VALUE(newvalue), GRN_TEXT_LEN(newvalue), &record, GRN_TRUE);
+  } else if (newvalue->header.type == GRN_UVECTOR) {
+    record = *newvalue;
+  }
+
+  if (is_string(domain) || newvalue->header.type == GRN_UVECTOR) {
+      GRN_RECORD_INIT(newvalue, GRN_OBJ_VECTOR, oldvalue->header.domain);
+    GRN_BULK_REWIND(&buf);
+    grn_inspect(ctx, &buf, &record);
+
+    n = grn_vector_size(ctx, &record);
+    for (i = 0; i < n; i++) {
+      grn_id tid;
+      grn_obj value;
+      tid = grn_uvector_get_element(ctx, &record, i, NULL);
+
+      GRN_UINT32_INIT(&value, 0);
+      grn_obj_get_value(ctx, column, tid, &value);
+      if (GRN_UINT32_VALUE(&value)) {
+        tid = GRN_UINT32_VALUE(&value);
+      }
+      grn_uvector_add_element(ctx, newvalue, tid, 0);
+      grn_obj_unlink(ctx, &value);
+    }
+  } else {
+    grn_id tid;
+    grn_obj value;
+    tid = GRN_RECORD_VALUE(newvalue);
+    GRN_UINT32_INIT(&value, 0);
+    grn_obj_get_value(ctx, column, tid, &value);
+    if (GRN_UINT32_VALUE(&value)) {
+      tid = GRN_UINT32_VALUE(&value);
+      GRN_BULK_REWIND(newvalue);
+      GRN_RECORD_SET(ctx, newvalue, tid);
+    }
+    grn_obj_unlink(ctx, &value);
+  }
+  grn_obj_unlink(ctx, &buf);
+  grn_obj_unlink(ctx, &record);
+
+  return NULL;
+}
+
+static grn_obj *
+command_tag_synonym_delete(grn_ctx *ctx, GNUC_UNUSED int nargs, GNUC_UNUSED grn_obj **args,
+                           grn_user_data *user_data)
+{
+  grn_obj *var, *table, *column;
+  unsigned int nhooks = 0;
+
+  char *table_name = NULL;
+  unsigned int table_len = 0;
+  char *column_name = NULL;
+  unsigned int column_len = 0;
+
+  var = grn_plugin_proc_get_var(ctx, user_data, "table", -1);
+  if (GRN_TEXT_LEN(var) != 0) {
+    table_name = GRN_TEXT_VALUE(var);
+    table_len = GRN_TEXT_LEN(var);
+  }
+  var = grn_plugin_proc_get_var(ctx, user_data, "column", -1);
+  if (GRN_TEXT_LEN(var) != 0) {
+    column_name = GRN_TEXT_VALUE(var);
+    column_len = GRN_TEXT_LEN(var);
+  }
+
+  table = grn_ctx_get(ctx, table_name, table_len);
+  column = grn_obj_column(ctx, table, column_name, column_len);
+
+  nhooks = grn_obj_get_nhooks(ctx, column, GRN_HOOK_SET);
+  if (nhooks) {
+    grn_obj *hook;
+    unsigned int i;
+    grn_obj data;
+    grn_obj buf;
+    GRN_TEXT_INIT(&buf, 0);
+    GRN_TEXT_INIT(&data, 0);
+    for (i=0; i < nhooks; i++) {
+      GRN_BULK_REWIND(&buf);
+      GRN_BULK_REWIND(&data);
+      hook = grn_obj_get_hook(ctx, column, GRN_HOOK_SET, i, &data);
+      grn_inspect_name(ctx, &buf, hook);
+      if (GRN_TEXT_LEN(&buf) == strlen("tag_synonym") &&
+          strncmp(GRN_TEXT_VALUE(&buf), "tag_synonym", GRN_TEXT_LEN(&buf)) == 0) {
+        grn_obj_delete_hook(ctx, column, GRN_HOOK_SET, 0);
+        nhooks = grn_obj_get_nhooks(ctx, column, GRN_HOOK_SET);
+        break;
+      }
+    }
+    grn_obj_unlink(ctx, &data);
+    grn_obj_unlink(ctx, &buf);
+  }
+  grn_ctx_output_array_open(ctx, "RESULT", 1);
+  grn_ctx_output_int32(ctx, nhooks);
+  grn_ctx_output_array_close(ctx);
+
+  return NULL;
+}
+
+static grn_obj *
+command_tag_synonym_add(grn_ctx *ctx, GNUC_UNUSED int nargs, GNUC_UNUSED grn_obj **args,
+                        grn_user_data *user_data)
+{
+  grn_obj *var, *proc, *table, *column;
+  unsigned int nhooks = 0;
+
+  char *table_name = NULL;
+  unsigned int table_len = 0;
+  char *column_name = NULL;
+  unsigned int column_len = 0;
+
+  var = grn_plugin_proc_get_var(ctx, user_data, "table", -1);
+  if (GRN_TEXT_LEN(var) != 0) {
+    table_name = GRN_TEXT_VALUE(var);
+    table_len = GRN_TEXT_LEN(var);
+  }
+  var = grn_plugin_proc_get_var(ctx, user_data, "column", -1);
+  if (GRN_TEXT_LEN(var) != 0) {
+    column_name = GRN_TEXT_VALUE(var);
+    column_len = GRN_TEXT_LEN(var);
+  }
+
+  table = grn_ctx_get(ctx, table_name, table_len);
+  column = grn_obj_column(ctx, table, column_name, column_len);
+
+  proc = grn_ctx_get(ctx, "tag_synonym", -1);
+  grn_obj_add_hook(ctx, column, GRN_HOOK_SET, 0, proc, 0);
+
+  grn_ctx_output_array_open(ctx, "RESULT", 1);
+  nhooks = grn_obj_get_nhooks(ctx, table, GRN_HOOK_INSERT);
+  grn_ctx_output_int32(ctx, nhooks);
+  grn_ctx_output_array_close(ctx);
+
+  return NULL;
+}
+
+grn_rc
+GRN_PLUGIN_INIT(GNUC_UNUSED grn_ctx *ctx)
+{
+  return GRN_SUCCESS;
+}
+
+grn_rc
+GRN_PLUGIN_REGISTER(grn_ctx *ctx)
+{
+  grn_expr_var vars[2];
+
+  grn_plugin_command_create(ctx, "tag_synonym", -1, command_tag_synonym, 0, vars);
+
+  grn_plugin_expr_var_init(ctx, &vars[0], "table", -1);
+  grn_plugin_expr_var_init(ctx, &vars[1], "column", -1);
+  grn_plugin_command_create(ctx, "tag_synonym_add", -1, command_tag_synonym_add, 2, vars);
+
+  grn_plugin_expr_var_init(ctx, &vars[0], "table", -1);
+  grn_plugin_expr_var_init(ctx, &vars[1], "column", -1);
+  grn_plugin_command_create(ctx, "tag_synonym_delete", -1, command_tag_synonym_delete, 2, vars);
+
+  return ctx->rc;
+}
+
+grn_rc
+GRN_PLUGIN_FIN(GNUC_UNUSED grn_ctx *ctx)
+{
+  return GRN_SUCCESS;
+}
